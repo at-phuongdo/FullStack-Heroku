cf45c54e68d7b767dfede1ca834c07239d212fcf
do-phuong <phuong@dactech.vn>
2019-12-27 16:30:06 +0700

    add project

---
+++ nodejs-and-express/build/precache-manifest.9f1c31b292efbecbccbb4dd0931f175f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883e3d6059240d563e352a2a0d0973d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af9898134f7e3e75b36f",
+    "url": "/static/css/main.6b25e63b.chunk.css"
+  },
+  {
+    "revision": "f42a9a922b6083f49049",
+    "url": "/static/js/2.1d30c23d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.1d30c23d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af9898134f7e3e75b36f",
+    "url": "/static/js/main.72559e4d.chunk.js"
+  },
+  {
+    "revision": "6fb4929d7017820d96ac",
+    "url": "/static/js/runtime-main.960a687c.js"
+  }
+]);
---
+++ saving-data-to-mongo-db/build/precache-manifest.9d062eadb00e3fe8bec7229713d0f94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc95e2517233df80f08ba7a91f7ddbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f5867522758d37a0cd",
+    "url": "/static/css/main.6b25e63b.chunk.css"
+  },
+  {
+    "revision": "f42a9a922b6083f49049",
+    "url": "/static/js/2.1d30c23d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.1d30c23d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18f5867522758d37a0cd",
+    "url": "/static/js/main.c4bf3ab6.chunk.js"
+  },
+  {
+    "revision": "6fb4929d7017820d96ac",
+    "url": "/static/js/runtime-main.960a687c.js"
+  }
+]);
